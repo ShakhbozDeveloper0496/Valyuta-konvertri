e010da533ea5224cfc2087e6823b15cfbdb64d76
Shaxboz <85881220+ShakhbozDeveloper0496@users.noreply.github.com>
2024-02-24 13:36:40 +0500

    this is convert to uzb from usd

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//  Valyuta konvertri
+
+using System.Runtime.InteropServices;
+using System.Security.Authentication.ExtendedProtection;
+
+System.Console.WriteLine("qiymatni kiriting: ");
+double qiymat= Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("valyuta qiymatini kiriting: ");
+double valyuta= Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine($"Qiymat= {qiymat} kurs= {valyuta}");
+System.Console.WriteLine($"{qiymat*valyuta} so\'m");
